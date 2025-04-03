92b3ca6fba9b36e72f59d18e008f806b31094ccb
Aaron Garcia <axyscreativeagency@gmail.com>
2025-04-02 20:43:12 -0600

    join carousel fix for single image

--- src/static/javascript/pages/join.js
+++ src/static/javascript/pages/join.js
@@ -1,22 +1,21 @@
 const joinCarousel = document.querySelector(".join-carousel");
 const joinCarouselImages = document.querySelectorAll(".join-carousel__img");
-let currentIndex = 1;
 
-if (joinCarousel) {
+if (joinCarouselImages.length > 1) {
+  let currentIndex = 0;
   let autoplayInterval =
     parseInt(joinCarousel.getAttribute("data-carousel-interval"), 10) || 5000;
 
-  if (joinCarouselImages.length > 0) {
-    joinCarouselImages[0].classList.add("active");
-  }
+  joinCarouselImages[currentIndex].classList.add("active");
 
   const updateSlides = () => {
-    joinCarouselImages.forEach((slide) => slide.classList.remove("active"));
-
-    joinCarouselImages[currentIndex].classList.add("active");
-
+    joinCarouselImages[currentIndex].classList.remove("active");
     currentIndex = (currentIndex + 1) % joinCarouselImages.length;
+    joinCarouselImages[currentIndex].classList.add("active");
   };
 
   setInterval(updateSlides, autoplayInterval);
+} else if (joinCarouselImages.length === 1) {
+  // Just show the single image
+  joinCarouselImages[0].classList.add("active");
 }
